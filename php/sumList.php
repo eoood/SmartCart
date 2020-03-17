0ed135f35dfaf61335aaac8b311a0ae35a4fe93e
hyuk2516 <61953208+hyuk2516@users.noreply.github.com>
2020-03-17 21:53:00 +0900

    php

--- php/sumList.php
+++ php/sumList.php
@@ -1,13 +1,18 @@
 <?php
 
- $con = mysqli_connect("localhost", "root", "apmsetup", "smartcart");
- $result = mysqli_query($con,"SUM(price) FROM ORDER2 where buyday = '2020-03-16';");
+ $con = mysqli_connect("localhost","oaoopw","kara1136!","oaoopw");
+ $result = mysqli_query($con,"SELECT SUM(price) FROM ORDER2 where buyday;");
 
  $response = array();
 
-array_push($response,array($result));
+ while($row = mysqli_fetch_array($result)){
+	
+ array_push($response,array("SUM"=>$row[0]));
 
+ }
+ 
  echo json_encode(array("response" =>$response));
+
  mysqli_close($con);
 
 ?>
