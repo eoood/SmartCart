3c25ac62f29348c48344312736b0038f1fce20b7
pdw624 <61953662+pdw624@users.noreply.github.com>
2020-03-24 23:59:57 +0900

    555

--- AndroidApp/v18_0324_edg/app/src/main/java/com/example/smartcart/ShoppingList.java
+++ AndroidApp/v18_0324_edg/app/src/main/java/com/example/smartcart/ShoppingList.java
@@ -22,13 +22,16 @@ import org.json.JSONObject;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class ShoppingList extends AppCompatActivity {
 
     //하단 메뉴바 버튼
     private Button btn_stock,btn_userinfo,btn_sales,btn_home;
 
-
+    private Button btn_order;
     //총액 나타내는 텍스트뷰에 임시로 바코드 값 표기 (ListView 활용 불가 ㅠ)
     private TextView pay;
 
@@ -38,7 +41,11 @@ public class ShoppingList extends AppCompatActivity {
     static String barcode;
     static String cartNum;
 
+    //바코드 값을 주면 상품명, 가격을 넘겨받는 변수
     static String db_value_pdInfo;
+
+    //id,상품명,가격,날짜 순으로 저장할 변수
+    ArrayList<String> orderList = new ArrayList<>();
 
     private DataOutputStream dos;
     private DataInputStream dis;
@@ -50,7 +57,7 @@ public class ShoppingList extends AppCompatActivity {
         setContentView(R.layout.activity_shopping_list);
 
         pay = (TextView) findViewById((R.id.pay));
-
+        btn_order = (Button) findViewById(R.id.btn_order);
         //setListView();
         //MainThreadException을 막기 위해 readBarcode()함수를 새로운 쓰레드에서 돌림
         new Thread() {
@@ -105,6 +112,14 @@ public class ShoppingList extends AppCompatActivity {
             }
         });
 
+        btn_order.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for(int j=0; j<orderList.size();j++){
+                    System.out.println(orderList.get(j));
+                }
+            }
+        });
 
 
 
@@ -199,7 +214,7 @@ public class ShoppingList extends AppCompatActivity {
                         for (int i = 0; i < obj.length(); i++) {
                             JSONObject user = obj.getJSONObject(i);
 
-                            result += user.getString("name") + "/" + user.getString("price");
+                            result += user.getString("name") + " " + user.getString("price");
                             Log.d("Response", "getstring : " + result);
                             //db_value_pdInfo = result;//상품명과 가격 저장
                             //Log.d("db_value_pdInfo", db_value_pdInfo);
@@ -207,8 +222,14 @@ public class ShoppingList extends AppCompatActivity {
 
                             db_value_pdInfo = result;
 
+                            SimpleDateFormat sdf = new SimpleDateFormat ( "yyyy-MM-dd");
+                            Date time = new Date();
+                            String currentTime = sdf.format(time);
 
-                            Log.d("db_value_pdInfo", db_value_pdInfo);
+                            String orderTemp = UserInfo.id + " " +db_value_pdInfo+" "+currentTime;
+                            orderList.add(orderTemp);
+
+                            Log.d("orderTemp", orderTemp);
                         }
 
                         Message msg = handler.obtainMessage();
@@ -218,15 +239,15 @@ public class ShoppingList extends AppCompatActivity {
                         //Log.d("world", result);
 
                         //다른 화면으로 넘겨주기 위한 코드(지금은 필요 없음, 바코드 값을 change_information 클래스에서 사용할 수 있도록 이동시킴)
-                        Intent intent = new Intent(ShoppingList.this, change_information.class);
+                        /*Intent intent = new Intent(ShoppingList.this, change_information.class);
                         intent.putExtra("barcode", barcode);
-                        startActivity(intent);
+                        startActivity(intent);*/
                         Log.d("Response", "intent");
-                    } else {
+                    } /*else {
                         Intent intent = new Intent(ShoppingList.this, change_information.class);
                         intent.putExtra("barcode", barcode);
                         startActivity(intent);
-                    }
+                    }*/
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -236,7 +257,6 @@ public class ShoppingList extends AppCompatActivity {
         RequestQueue queue = Volley.newRequestQueue(ShoppingList.this);
         queue.add(barcodeRequest);
         Log.d("Response", "getstring1 : " + barcode);
-
 
     }
 
